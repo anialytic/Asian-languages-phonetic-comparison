8f804c42eea043a5809a1d3d926e90fca7326cd9
Ania <anna.kors.19.1@gmail.com>
2025-08-13 14:18:29 +0300

    New file

---
+++ syl_comparison.py
@@ -0,0 +1,20 @@
+# відкрити файл
+import pandas as pd
+df = pd.read_excel("Dictionary-mini.xlsx", engine='openpyxl')
+print(df)
+
+# всі колонки з кількістю складів
+syllable_columns = [col for col in df.columns if "syllable" in col.lower()]
+
+# тільки ці колонки
+columns_to_show = ["syllable_zh", "syllable_ca", "syllable_ja", "syllable_ko", "Syllable_vi"]
+
+# перевірка збігу в рядку
+def has_syllable_match(row):
+    values = row[columns_to_show].tolist()
+    # чи є повтори
+    return len(values) != len(set(values))
+
+#новий стовпчик із результатом
+df["Has_match"] = df.apply(has_syllable_match, axis=1)
+print(df[columns_to_show + ["Has_match"]])
