e1b7e34221f09b522403f967e338a63e2d7797cf
Ania <anna.kors.19.1@gmail.com>
2025-08-18 20:03:35 +0300

    Add Chao_tone_letters.py

---
+++ Chao_tone_letters.py
@@ -0,0 +1,13 @@
+tones_zh = input('Please enter pinyin:')
+
+# пройтись циклом по списку?
+# tones_list = []
+if "ā" in tones_zh or "ī" in tones_zh or "ū" in tones_zh or "ē" in tones_zh or "ō" in tones_zh or "ǖ" in tones_zh:
+	print(55)
+if "á" in tones_zh or "í" in tones_zh or "ú" in tones_zh or "é" in tones_zh or "ó" in tones_zh or "ǘ" in tones_zh:
+	print(35)
+if "ǎ" in tones_zh or "ǐ" in tones_zh or "ǔ" in tones_zh or "ě" in tones_zh or "ǒ" in tones_zh or "ǚ" in tones_zh:
+	print(214)
+if "à" in tones_zh or "ì" in tones_zh or "ù" in tones_zh or "è" in tones_zh or "ò" in tones_zh or "ǜ" in tones_zh:
+	print(51)
+
