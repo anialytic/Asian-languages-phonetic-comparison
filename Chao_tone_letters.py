9b1a5ba5c810a52513ef859fb42ff5fa43aaff7f
Ania <anna.kors.19.1@gmail.com>
2025-09-04 18:51:32 +0300

    Add jyutping to Chao_tone_letters.py

--- Chao_tone_letters.py
+++ Chao_tone_letters.py
@@ -10,4 +10,18 @@ if "ǎ" in tones_zh or "ǐ" in tones_zh or "ǔ" in tones_zh or "ě" in tones_zh 
 	print(214)
 if "à" in tones_zh or "ì" in tones_zh or "ù" in tones_zh or "è" in tones_zh or "ò" in tones_zh or "ǜ" in tones_zh:
 	print(51)
-
+  
+jyutping = input('Please enter jyutping:')  
+  # tones_list = [] 
+if "1" in jyutping:
+	print(55)
+if "2" in jyutping:
+	print(25)
+if "3" in jyutping:
+	print(33)
+if "4" in jyutping:
+	print(21)
+if "5" in jyutping:
+	print(23)
+if "6" in jyutping:
+	print(22)
